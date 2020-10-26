39e67d2c3b7bf89a6f4c3a98dd8848f8a49f4dbf
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-10-26 15:17:34 -0700

    Main Page created

---
+++ react-front-end/src/components/Employees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Button from '@material-ui/core/Button'
+
+export default function Employees(props) {
+  return (
+    <Button onClick={props.onClick}>Employees</Button>
+  )
+}
--- react-front-end/src/components/HeaderMenu.jsx
+++ react-front-end/src/components/HeaderMenu.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
+import Profile from './Profile'
+import Employees from './Employees'
+import Work from './Work'
+import EmployeesGroupTabs from './EmployeesGroupTabs'
+import ProfileTabs from './ProfileTabs'
+import WorkTabs from './WorkTabs'
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -58,6 +61,33 @@ function ResponsiveDrawer(props) {
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  const [state, setState] = React.useState({
+    isManager: true,
+    isActiveProfile: false,
+    isActiveEmployees:false,
+    isActiveWork:false
+      
+    
+  })
+  const activateProfile = () => setState({
+    ...state,
+    isActiveProfile: true,
+    isActiveEmployees: false,
+    isActiveWork: false
+  })
+  const activateEmployees = () => setState({
+    ...state,
+    isActiveEmployees: true,
+    isActiveProfile: false,
+    isActiveWork: false
+  })
+  const activateWork = () => setState({
+    ...state,
+    isActiveWork: true,
+    isActiveProfile: false,
+    isActiveEmployees: false
+  })
+  
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
@@ -67,22 +97,17 @@ function ResponsiveDrawer(props) {
       <div className={classes.toolbar} />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+      <ListItem button>
+        <Profile onClick={activateProfile}/>
+      </ListItem>
+      <ListItem button>
+        <Employees onClick={activateEmployees}/>
+      </ListItem>
+      <ListItem button>
+        <Work onClick={activateWork} />
+      </ListItem>
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
 
@@ -103,7 +128,7 @@ function ResponsiveDrawer(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Responsive drawer
+            Management Application
           </Typography>
         </Toolbar>
       </AppBar>
@@ -140,29 +165,11 @@ function ResponsiveDrawer(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        {state.isActiveEmployees &&  <EmployeesGroupTabs />}
+        {state.isActiveProfile && <ProfileTabs />}
+        {state.isActiveWork && <WorkTabs />}
+
+        {/* {state.isManager && <ManagerTabs />} */}
       </main>
     </div>
   );
