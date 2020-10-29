0bdd0a54515705ae5805f322caa4d1b353d4a78c
henryhe1234 <henryhe1123@gmail.com>
2020-10-28 21:10:03 -0600

    whaterer

--- express-back-end/data/queryMethod/group.js
+++ express-back-end/data/queryMethod/group.js
@@ -1,1 +1,31 @@
 const db = require('../db.js')
+
+const getGroups = ()=>{
+  return db('tasks')
+  .join('employee_tasks','tasks.id','employee_tasks.task_id')
+  .join('employees','employees.id','employee_tasks.employee_id')
+  .then((result)=>{
+    console.log(result)
+    return result
+  })
+}
+const getGroupById = (id)=>{
+  return db('tasks')
+  .where('tasks.id',id)
+  .join('employee_tasks','tasks.id','=','employee_tasks.task_id')
+  .join('employees','employees.id','=','employee_tasks.employee_id')
+  .then((result)=>{
+    return result
+  })
+}
+const changeGroup = (employeeId,currentTaskId,newTaskId)=>{
+  return db('')
+  .join('employees','employees.id','=','employee_tasks.task_id')
+
+}
+
+module.exports = {
+  getGroups,
+  getGroupById,
+  changeGroup
+};
---
+++ express-back-end/routes/group.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const { getGroups, getGroupById } = require("../data/queryMethod/group");
+
+router.get("/", (req, res) => {
+  getGroups().then((result) => {
+    // console.log(result)
+    let tasks = {};
+    for (const data of result) {
+      if (!tasks[data.task_id]) {
+        tasks[data.task_id] = {
+          taskId: data.task_id,
+          taskName: data.name,
+          taskContent: data.content,
+          members: [
+            {
+              employeeId: data.employee_id,
+              employeeFirstName: data.first_name,
+              employeeLastName: data.last_name,
+            },
+          ],
+        };
+      } else {
+        tasks[data.task_id].members.push({
+          employeeId: data.employee_id,
+          employeeFirstName: data.first_name,
+          employeeLastName: data.last_name,
+        });
+      }
+      //   if(!tasks[data.id]){
+      //     tasks[data.id] = {name:data.name,employees:[{first_name:data.first_name,last_name:data.last_name}],status:data.status}
+
+      //   }else{
+      //     tasks[data.id].employees.push({first_name:data.first_name,last_name:data.last_name})
+      //   }
+    }
+    res.json(tasks);
+  });
+});
+router.get("/1", (req, res) => {
+  getGroupById(1).then((result) => {
+    console.log(result);
+    let tasks = {};
+    for (const data of result) {
+      if (!tasks[data.id]) {
+        tasks[data.id] = {
+          name: data.name,
+          employees: [
+            { first_name: data.first_name, last_name: data.last_name },
+          ],
+          status: data.status,
+        };
+      } else {
+        tasks[data.id].employees.push({
+          first_name: data.first_name,
+          last_name: data.last_name,
+        });
+      }
+    }
+    res.json(tasks);
+  });
+});
+
+module.exports = router;
