59d86e9d9b7cbffc4e70ba46b41bcef8373c792b
henryhe1234 <henryhe1123@gmail.com>
2020-10-29 14:34:11 -0600

    no way

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,36 +1,31 @@
-import './App.css';
+import "./App.css";
 import { useSelector } from "react-redux";
-
+import React from "react";
 import allActions from "./actions";
-
-import HeaderMenu from "./components/Menu"
-
-import SignIn from './components/Sign-in'
-
+import ManagerMenu from "./components/ManagerMenu";
+import EmployeeMenu from "./components/EmployeeMenu";
+import SignIn from "./components/ManagerSign-in";
+import EmployeeSignIn from "./components/EmployeeSign-in";
 function App() {
-
-  const login = useSelector((state)=> state.currentUser.loggedIn)
-  const manager = useSelector((state) => state.managerLogin.manager)
-  
-  const renderPage = 
-    manager ? (
-      login ? (
-        <ManagerMenu/>
-      ) : (
-        <SignIn action={allActions.userActions} changeLogin={allActions.managerLoginAction} />
-      )
+  const login = useSelector((state) => state.currentUser.loggedIn);
+  const manager = useSelector((state) => state.managerLogin.manager);
+  const renderPage = manager ? (
+    login ? (
+      <ManagerMenu />
     ) : (
-        login ? (
-          <EmployeeMenu />
-        ) : (
-          <EmployeeSignIn action={allActions.userActions} changeLogin={allActions.managerLoginAction} />
-        )
-      )
-  return (
-    <div className="App">
-      {renderPage}
-    </div>
+      <SignIn
+        action={allActions.userActions}
+        changeLogin={allActions.managerLoginAction}
+      />
+    )
+  ) : login ? (
+    <EmployeeMenu />
+  ) : (
+    <EmployeeSignIn
+      action={allActions.userActions}
+      changeLogin={allActions.managerLoginAction}
+    />
   );
+  return <div className="App">{renderPage}</div>;
 }
-
 export default App;
