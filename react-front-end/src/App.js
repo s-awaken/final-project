cb15803fa7c802143ceafcb54e7e851743789630
henryhe1234 <henryhe1123@gmail.com>
2020-10-29 14:36:14 -0600

    enough

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import { useSelector } from "react-redux";
 import React from "react";
 import allActions from "./actions";
+
 import ManagerMenu from "./components/ManagerMenu";
 import EmployeeMenu from "./components/EmployeeMenu";
+
 import SignIn from "./components/ManagerSign-in";
 import EmployeeSignIn from "./components/EmployeeSign-in";
+
 function App() {
   const login = useSelector((state) => state.currentUser.loggedIn);
   const manager = useSelector((state) => state.managerLogin.manager);
+
   const renderPage = manager ? (
     login ? (
       <ManagerMenu />
@@ -28,4 +32,5 @@ function App() {
   );
   return <div className="App">{renderPage}</div>;
 }
+
 export default App;
