4aa71c4c00906203760d9fe4a1b74e41230ade26
henryhe1234 <henryhe1123@gmail.com>
2020-10-29 13:06:09 -0600

    finish userquery

--- express-back-end/data/queryMethod/user.js
+++ express-back-end/data/queryMethod/user.js
@@ -1,1 +1,19 @@
 const db = require('../db.js')
+
+const getEmployeesByDepartment = (department_id)=>{
+  return db('employees')
+  .where(function(){
+    this.where('department_id',department_id)
+    .andWhere('is_manager',false)
+    
+  })
+  .then((result)=>{
+    return result
+  })
+
+}
+
+module.exports = {
+  getEmployeesByDepartment
+
+};
